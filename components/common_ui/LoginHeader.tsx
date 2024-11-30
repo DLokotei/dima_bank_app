2e007b4ed9cd83836962ba19e0f17c8d77a74089
Dmytro.Lokotei <dimzyun@gmail.com>
2024-11-30 22:47:35 +0200

    * added context provider for user data

--- components/common_ui/LoginHeader.tsx
+++ components/common_ui/LoginHeader.tsx
@@ -1,8 +1,25 @@
-import React from "react"
-import { Text } from "react-native"
+import { UserContext, UserContext2 } from "@/feature/auth/userProfileData"
+import React, { useContext, useEffect } from "react"
+import { View, Text } from "react-native"
 
 const LoginHeader = () => {
-    return <Text>Hello UserName</Text>
+    const userData = useContext(UserContext);
+    const userData2 = useContext(UserContext2);
+
+    useEffect(
+        () => {
+            userData?.setUserName('New User Name')
+            userData2.betsStatus = 'inProgress'
+        },
+        [] // call only on first render
+    )
+
+    return (
+        <View>
+            <Text>Hello {userData?.userName ?? 'nil'}</Text>
+            <Text>bet status: {userData2?.betsStatus ?? 'nil'}</Text>
+        </View>
+    )
 }
 
 export default LoginHeader
--- components/navigation/app_navigation_buttons.tsx
+++ components/navigation/app_navigation_buttons.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Text, Button, View, Pressable } from "react-native"
+import { Text, Button, View, Pressable, TouchableOpacity } from "react-native"
 import { Link, router } from 'expo-router';
 
 const AppRoutesView = () => {
@@ -12,8 +12,30 @@ const AppRoutesView = () => {
             <Pressable onPress={()=> {router.navigate('/screen_component_props_demo')}}>
                 <Text>Route screen_component_props_demo</Text>
             </Pressable>
+            <TouchableOpacity onPress={()=> {
+                router.push('/screen_button_callback')
+            }}>
+                <Text>Route push screen_button_callback</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=> {
+                router.push('/screen_user_profile')
+            }}>
+                <Text>Route push screen_user_profile</Text>
+            </TouchableOpacity>
             <Link href="/screen_list_demo">Route screen_list_demo</Link>
             <Link href="/screen_drawer_test">Route screen_drawer_test</Link>
+            <TouchableOpacity onPress={()=> {
+                let canGoBack = router.canGoBack()
+                if (canGoBack) {
+                    router.back()
+                } else {
+                    router.dismissTo('/')
+                    console.warn('cant go back')
+                }
+                
+            }}>
+                <Text>GO BACK</Text>
+            </TouchableOpacity>
         </View>
     )
 }
